88f26cc24ff4841e0b328d42262a6fd09cacdd7a
zimolab <zimolab@aliyun.com>
2025-10-12 22:28:06 +0800

    add example code

---
+++ examples/hello_world.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+from pyguiadapterlite import GUIAdapter, uprint
+
+
+def temperature_converter(
+    temperature: float,
+    from_unit: Literal["°C", "°F"] = "°F",
+    to_unit: Literal["°C", "°F"] = "°C",
+) -> float:
+    """
+    Convert temperature from one unit to another.
+    ---
+    *This is a simple demo of [`PyGUIAdapterLite`](https://github.com/zimolab/PyGUIAdapterLite).*
+
+    Args:
+        temperature (flot): current value of temperature
+        from_unit (str): current unit of temperature
+        to_unit (str):  target unit of temperature
+
+    Returns:
+        the converted value of temperature
+    """
+    if from_unit == "°C" and to_unit == "°F":
+        converted = temperature * 9 / 5 + 32
+    elif from_unit == "°F" and to_unit == "°C":
+        converted = (temperature - 32) * 5 / 9
+    else:
+        converted = temperature
+    uprint(f"{temperature}{from_unit} = {converted}{to_unit}")
+    return converted
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    adapter.add(temperature_converter)
+    adapter.run()
