8a6c3b4a40cc2023939b3d21ccfdc2323164756e
zimolab <zimolab@aliyun.com>
2025-10-12 18:36:06 +0800

    update example code

--- examples/enum_type_demo.py
+++ examples/enum_type_demo.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from pyguiadapterlite import GUIAdapter, uprint
+from pyguiadapterlite.types.choices.enumchoice import EnumValue
 
 
 class Color(Enum):
@@ -13,12 +14,43 @@ class Fruit(Enum):
     APPLE, BANANA, ORANGE, MANGO = range(4)
 
 
-def enum_type_demo(arg1: Color, fruit: Fruit = Fruit.ORANGE):
-    uprint(f"arg1: {arg1}")
+class Weekday(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+
+def enum_type_demo(color: Color, fruit: Fruit, day: Weekday = Weekday.SUNDAY):
+    """
+    Demo for enum types.
+
+    :param color: parameter with enum type Color
+    :param fruit: parameter with enum type Fruit, configured with widget_configs parameter of GUIAdapter.add()
+    :param day: parameter with enum type Weekday, with default value SUNDAY
+    :return:
+    """
+    uprint(f"arg1: {color}")
     uprint(f"fruit: {fruit}")
+    uprint(f"day: {day}")
 
 
 if __name__ == "__main__":
     adapter = GUIAdapter()
-    adapter.add(enum_type_demo)
+    adapter.add(
+        enum_type_demo,
+        widget_configs={
+            "fruit": EnumValue(
+                default_value=Fruit.BANANA,
+                description="parameter with enum type Fruit, configured with widget_configs parameter of GUIAdapter.add()",
+                columns=2,
+                hide_label=False,
+                content_title="Your favorite fruit",
+                enum_class=Fruit,
+            )
+        },
+    )
     adapter.run()
