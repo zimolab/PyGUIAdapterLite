98c0841ed3f5fafb1adde82863206f15ebcbd8bd
zimolab <zimolab@aliyun.com>
2025-10-14 00:11:36 +0800

    impl progressbar and made its apis available to the user function

--- pyguiadapterlite/__init__.py
+++ pyguiadapterlite/__init__.py
@@ -1,6 +1,17 @@
 from pyguiadapterlite.core.adapter import GUIAdapter
-from pyguiadapterlite.core.context import uprint, is_function_cancelled
 from pyguiadapterlite.core.fn import ParameterError
 from pyguiadapterlite.windows.basewindow import BaseWindowConfig, BaseWindow
 from pyguiadapterlite.windows.fnexecwindow import FnExecuteWindowConfig, FnExecuteWindow
 from pyguiadapterlite.windows.fnselectwindow import FnSelectWindowConfig, FnSelectWindow
+from pyguiadapterlite.core.context import (
+    uprint,
+    is_function_cancelled,
+    is_cancel_requested,
+    is_progressbar_enabled,
+    is_progress_label_enabled,
+    start_progressbar,
+    stop_progressbar,
+    update_progressbar,
+    show_progressbar,
+    hide_progressbar,
+)
