34c485007e1c8859198cb92612ad45be60bd6c8b
zimolab <zimolab@aliyun.com>
2025-10-13 23:22:21 +0800

    move ParameterError class to core/fn.py

--- pyguiadapterlite/core/context.py
+++ pyguiadapterlite/core/context.py
@@ -1,21 +1,9 @@
-from typing import Any
+from concurrent.futures import Future
+from typing import Any, Callable
 
+from pyguiadapterlite.windows.fnexecwindow import FnExecuteWindow
 from pyguiadapterlite.core.ucontext import UContext
-
-
-class ParameterError(Exception):
-    def __init__(self, parameter_name: str, message: str):
-        self._parameter_name: str = parameter_name
-        self._message: str = message
-        super().__init__(message)
-
-    @property
-    def parameter_name(self) -> str:
-        return self._parameter_name
-
-    @property
-    def message(self) -> str:
-        return self._message
+from pyguiadapterlite.utils import _error
 
 
 def is_cancel_requested() -> bool:
@@ -33,6 +21,29 @@ def uprint(*messages: Any, sep=" ", end="\n"):
         exec_window.output_view.write(f"{message}{sep}")
     if end:
         exec_window.output_view.write(end)
+
+
+def _run_on_ui_thread(
+    func: Callable[[FnExecuteWindow, Future], Any], *args, **kwargs
+) -> Any:
+    exec_window = UContext.current_execute_window()
+    if not exec_window:
+        raise RuntimeError("fn execute_window is not set")
+    future = Future()
+
+
+def is_progressbar_enabled() -> bool:
+    exec_window = UContext.current_execute_window()
+    if not exec_window:
+        _error("fn execute_window is not set")
+        return False
+    future = Future()
+
+    def callback():
+        future.set_result(exec_window.is_progressbar_enabled())
+
+    exec_window.parent.after(0, callback)
+    return future.result()
 
 
 is_function_cancelled = is_cancel_requested
--- pyguiadapterlite/core/fn.py
+++ pyguiadapterlite/core/fn.py
@@ -84,3 +84,18 @@ class FnInfo(object):
         if self.fn_name:
             return self.fn_name
         return self.fn.__name__
+
+
+class ParameterError(Exception):
+    def __init__(self, parameter_name: str, message: str):
+        self._parameter_name: str = parameter_name
+        self._message: str = message
+        super().__init__(message)
+
+    @property
+    def parameter_name(self) -> str:
+        return self._parameter_name
+
+    @property
+    def message(self) -> str:
+        return self._message
