e74b0fc1673c70e421e036fc9b4601c6adb4478b
zimolab <zimolab@aliyun.com>
2025-10-13 18:06:02 +0800

    add example code

---
+++ examples/equation_solver.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from pyguiadapterlite import GUIAdapter, uprint, ParameterError
+
+
+def equation_solver(a: float = 1.0, b: float = 0.0, c: float = 0.0) -> Optional[tuple]:
+    """A simple equation solver for equations like:
+
+    ax^2 + bx + c = 0 (a, b, c ∈ R and a ≠ 0)
+
+    @param a: a ∈ R and a ≠ 0
+    @param b: b ∈ R
+    @param c: c ∈ R
+    @return:
+    """
+    if a == 0:
+        raise ParameterError(parameter_name="a", message="a cannot be zero!")
+    uprint(f"Equation:")
+    uprint(f"  {a}x² + {b}x + {c} = 0")
+    delta = b**2 - 4 * a * c
+    if delta < 0:
+        return None
+    x1 = (-b + delta**0.5) / (2 * a)
+    if delta == 0:
+        return x1, x1
+    x2 = (-b - delta**0.5) / (2 * a)
+    return x1, x2
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    adapter.add(equation_solver)
+    adapter.run()
