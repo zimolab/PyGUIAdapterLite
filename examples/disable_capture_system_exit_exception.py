c73eb1caf7c73c5d5d9c831e15b7bc769455b0d1
zimolab <zimolab@aliyun.com>
2025-10-14 19:47:05 +0800

    add example code

---
+++ examples/disable_capture_system_exit_exception.py
@@ -0,0 +1,13 @@
+from pyguiadapterlite import GUIAdapter
+
+
+def test_fix(a: int, b: int):
+    print("Test Fix 1")
+    exit(-1)
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    # adapter.add(test_fix, capture_system_exit_exception=True)
+    adapter.add(test_fix, capture_system_exit_exception=False)
+    adapter.run()
