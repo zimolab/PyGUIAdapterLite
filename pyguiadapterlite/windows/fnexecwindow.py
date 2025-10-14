a99c931e3c8c09be12b89e6a51772975385fe302
zimolab <zimolab@aliyun.com>
2025-10-14 23:08:13 +0800

    add load/save parameters interfaces

--- pyguiadapterlite/windows/fnexecwindow.py
+++ pyguiadapterlite/windows/fnexecwindow.py
@@ -1,7 +1,9 @@
 import dataclasses
-from tkinter import Tk, Toplevel, Frame, messagebox, BooleanVar
+import json
+from pathlib import Path
+from tkinter import Tk, Toplevel, Frame, messagebox, BooleanVar, filedialog
 from tkinter.ttk import Button, Checkbutton, Progressbar, Label
-from typing import Union, Optional, Any, cast, Dict, Literal
+from typing import Union, Optional, Any, cast, Dict, Literal, Callable
 
 from pyguiadapterlite._messages import (
     MSG_WARNING_TITLE,
@@ -20,6 +22,16 @@ from pyguiadapterlite._messages import (
     MSG_FUNC_NOT_EXECUTING,
     MSG_EXCEPTION_DURING_EXEC,
     MSG_FUNC_RET_MSG,
+    MSG_PARAMS_SERIALIZATION_FAILED,
+    MSG_SAVE_FILE_DIALOG_TITLE,
+    MSG_SAVE_PARAMS_FAILED,
+    MSG_SAVE_PARAMS_SUCCESS,
+    MSG_LOAD_FILE_DIALOG_TITLE,
+    MSG_LOAD_PARAMS_FAILED,
+    MSG_PARAMS_DESERIALIZATION_FAILED,
+    MSG_SET_PARAMS_FAILED,
+    MSG_LOAD_PARAMS_SUCCESS,
+    MSG_INVALID_PARAMS_NOT_APPLIED,
 )
 from pyguiadapterlite.components.paramtabview import (
     ParameterGroupTabView,
@@ -29,8 +41,8 @@ from pyguiadapterlite.components.scrollarea import ParameterWidgetArea
 from pyguiadapterlite.components.termview import TermView
 from pyguiadapterlite.components.textview import TextView, SimpleTextViewer
 from pyguiadapterlite.components.valuewidget import InvalidValue
-from pyguiadapterlite.core.fn import ParameterError
 from pyguiadapterlite.core.fn import FnInfo, BaseFunctionExecutor, ExecuteStateListener
+from pyguiadapterlite.core.fn import ParameterError
 from pyguiadapterlite.core.threaded import ThreadedExecutor
 from pyguiadapterlite.core.ucontext import UContext
 from pyguiadapterlite.utils import (
@@ -41,6 +53,7 @@ from pyguiadapterlite.utils import (
     show_error,
     show_information,
     _info,
+    _exception,
 )
 from pyguiadapterlite.windows.basewindow import BaseWindowConfig, BaseWindow
 from pyguiadapterlite.windows.fnvalidationwindow import (
@@ -492,12 +505,123 @@ class FnExecuteWindow(BaseWindow, ExecuteStateListener):
         self._main_area.output_view.clear()
 
     def get_parameter_values(self) -> Dict[str, Any]:
-        parameter_values = self._main_area.get_parameter_values()
-        return parameter_values
+        return self._main_area.get_parameter_values()
 
-    def set_parameter_values(self, values: Dict[str, Any]):
-        ret = self._main_area.update_parameter_values(values)
-        return ret
+    def set_parameter_values(
+        self, values: Dict[str, Any], ignore_not_exist: bool = True
+    ):
+        return self._main_area.update_parameter_values(values, ignore_not_exist)
+
+    def save_parameter_values(
+        self,
+        save_path: Union[str, Path, None] = None,
+        serializer: Optional[Callable[[Dict[str, Any]], str]] = None,
+        **filedialog_args,
+    ):
+        self._close_param_validation_win()
+        current_values = self.get_parameter_values()
+        if not self.validate_parameter_values(current_values):
+            return
+        try:
+            serializer = serializer or self._default_params_serializer
+            serialized_text = serializer(current_values)
+        except BaseException as e:
+            _exception(e, "failed to serialize parameters")
+            show_error(MSG_PARAMS_SERIALIZATION_FAILED, parent=self.parent)
+            return
+        if not save_path:
+            filedialog_args = filedialog_args or {}
+            filedialog_args.pop("parent", None)
+            filedialog_args.setdefault("title", MSG_SAVE_FILE_DIALOG_TITLE)
+            filedialog_args.setdefault("confirmoverwrite", True)
+            filedialog_args.setdefault("initialdir", "./")
+            save_path = filedialog.asksaveasfilename(
+                parent=self.parent, **filedialog_args
+            )
+        if not save_path:
+            return
+        save_path = Path(save_path)
+        try:
+            with open(save_path, "w", encoding="utf-8") as f:
+                f.write(serialized_text)
+        except BaseException as e:
+            _exception(e, "failed to save parameters to file")
+            show_error(MSG_SAVE_PARAMS_FAILED, parent=self.parent)
+        else:
+            _info(f"parameters saved to file: {save_path.absolute().as_posix()}")
+            show_information(
+                MSG_SAVE_PARAMS_SUCCESS + f"\n{save_path.absolute().as_posix()}",
+                parent=self.parent,
+            )
+
+    def load_parameter_values(
+        self,
+        load_path: Union[str, Path, None] = None,
+        ignore_not_exist: bool = True,
+        deserializer: Optional[Callable[[str], Dict[str, Any]]] = None,
+        **filedialog_args,
+    ):
+        if not load_path:
+            filedialog_args.pop("parent", None)
+            filedialog_args.setdefault("title", MSG_LOAD_FILE_DIALOG_TITLE)
+            filedialog_args.setdefault("initialdir", "./")
+            load_path = filedialog.askopenfilename(
+                parent=self.parent, **filedialog_args
+            )
+
+        if not load_path:
+            return
+        load_path = Path(load_path)
+        try:
+            with open(load_path, "r", encoding="utf-8") as f:
+                serialized_text = f.read()
+        except BaseException as e:
+            _exception(e, "failed to load parameters from file")
+            show_error(MSG_LOAD_PARAMS_FAILED, parent=self.parent)
+            return
+
+        try:
+            deserializer = deserializer or self._default_params_deserializer
+            param_values = deserializer(serialized_text)
+        except BaseException as e:
+            _exception(e, "failed to deserialize parameters")
+            show_error(MSG_PARAMS_DESERIALIZATION_FAILED, parent=self.parent)
+            return
+
+        try:
+            ret = self.set_parameter_values(
+                param_values, ignore_not_exist=ignore_not_exist
+            )
+
+            if not self._check_invalid_parameters(result=ret):
+                return
+        except BaseException as e:
+            _exception(e, "failed to set parameter values")
+            show_error(MSG_SET_PARAMS_FAILED, parent=self.parent)
+        else:
+            _info(f"parameters loaded from file: {load_path.absolute().as_posix()}")
+            show_information(
+                MSG_LOAD_PARAMS_SUCCESS + f"\n{load_path.absolute().as_posix()}",
+                parent=self.parent,
+            )
+
+    def _check_invalid_parameters(self, result: Dict[str, Union[Any, InvalidValue]]):
+        invalid_params = {}
+        for param_name, value in result.items():
+            if isinstance(value, InvalidValue):
+                invalid_msg = value.msg or value.exception
+                label = self._get_label_for_parameter(param_name)
+                invalid_params[param_name] = (label, str(invalid_msg))
+        if not invalid_params:
+            return True
+
+        validation_wind_config = ParameterValidationWindowConfig(
+            font=self.config.document_font,
+            invalid_params_label_text=MSG_INVALID_PARAMS_NOT_APPLIED,
+        )
+
+        self._show_validation_window(invalid_params, validation_wind_config)
+        return False
 
     def validate_parameter_values(
         self, param_values: Dict[str, Union[Any, InvalidValue]]
@@ -523,23 +647,49 @@ class FnExecuteWindow(BaseWindow, ExecuteStateListener):
         del param_values
 
         if validation_errors:
-            self._close_param_validation_win()
-            self._param_validation_win_parent = Toplevel(self._parent)
-            self._param_validation_win = ParameterValidationWindow(
-                self._param_validation_win_parent,
-                validation_errors,
-                config=ParameterValidationWindowConfig(font=self.config.document_font),
+            self._show_validation_window(
+                invalid_params=validation_errors,
+                validation_wind_config=ParameterValidationWindowConfig(
+                    font=self.config.document_font
+                ),
             )
-            self._param_validation_win.set_on_close_handler(
-                self._on_param_validation_win_close
-            )
-            self._param_validation_win.set_item_clicked_handler(
-                self._on_param_validation_win_item_clicked
-            )
+            # self._close_param_validation_win()
+            # self._param_validation_win_parent = Toplevel(self._parent)
+            # self._param_validation_win = ParameterValidationWindow(
+            #     self._param_validation_win_parent,
+            #     validation_errors,
+            #     config=validation_window_config
+            #     or ParameterValidationWindowConfig(font=self.config.document_font),
+            # )
+            # self._param_validation_win.set_on_close_handler(
+            #     self._on_param_validation_win_close
+            # )
+            # self._param_validation_win.set_item_clicked_handler(
+            #     self._on_param_validation_win_item_clicked
+            # )
 
         if not validation_errors:
             return True
         return False
+
+    def _show_validation_window(
+        self,
+        invalid_params: Dict[Any, Any],
+        validation_wind_config: ParameterValidationWindowConfig,
+    ):
+        self._close_param_validation_win()
+        self._param_validation_win_parent = Toplevel(self._parent)
+        self._param_validation_win = ParameterValidationWindow(
+            self._param_validation_win_parent,
+            invalid_params=invalid_params,
+            config=validation_wind_config,
+        )
+        self._param_validation_win.set_on_close_handler(
+            self._on_param_validation_win_close
+        )
+        self._param_validation_win.set_item_clicked_handler(
+            self._on_param_validation_win_item_clicked
+        )
 
     def _get_label_for_parameter(self, fn_name: str) -> str:
         for fn_name1, config in self._fn_info.parameter_configs.items():
@@ -591,3 +741,11 @@ class FnExecuteWindow(BaseWindow, ExecuteStateListener):
 
     def _on_param_validation_win_item_clicked(self, param_name: str):
         self._main_area.show_error_effect(param_name)
+
+    @staticmethod
+    def _default_params_serializer(params: Dict[str, Any]) -> str:
+        return json.dumps(params, indent=2, ensure_ascii=False)
+
+    @staticmethod
+    def _default_params_deserializer(params_str: str) -> Dict[str, Any]:
+        return json.loads(params_str)
