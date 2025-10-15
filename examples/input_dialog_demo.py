8605288633e6e05550a1fe63b36490ab41130a2d
zimolab <zimolab@aliyun.com>
2025-10-15 21:30:35 +0800

    update example code

--- examples/input_dialog_demo.py
+++ examples/input_dialog_demo.py
@@ -1,8 +1,17 @@
-from pyguiadapterlite import uprint, GUIAdapter
-from pyguiadapterlite.core.context import (
+import os
+
+from pyguiadapterlite import (
+    uprint,
+    GUIAdapter,
+    get_path_input,
+    get_file_path_input,
+    get_dir_path_input,
+)
+from pyguiadapterlite import (
     get_string_input,
     get_int_input,
     get_float_input,
+    get_string_input2,
 )
 
 
@@ -17,6 +26,25 @@ def input_dialog_demo(
     uprint(f"Integer input: {ret}")
     ret = get_float_input(prompt=get_float_prompt)
     uprint(f"Float input: {ret}")
+    # input dialog implemented on custom dialog
+    ret = get_string_input2(label_text="Enter the password:", echo_char="*")
+    uprint(f"password: {ret}")
+    ret = get_path_input(
+        label_text="Select a file or directory:",
+        file_types=[("All files", "*.*"), ("Python files", "*.py")],
+        start_dir=os.getcwd(),
+    )
+    uprint(f"Selected: {ret}")
+    ret = get_file_path_input(
+        title="Select a file",
+        start_dir=os.getcwd(),
+        file_dialog_action="open",
+        file_dialog_title="Open file",
+        file_types=[("All files", "*.*"), ("Python files", "*.py")],
+    )
+    uprint(f"Selected file: {ret}")
+    ret = get_dir_path_input(title="Select a directory", start_dir=os.getcwd())
+    uprint(f"Selected directory: {ret}")
 
 
 if __name__ == "__main__":
