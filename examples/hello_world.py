d55e80b0d503eb696630f6724cfe0a864f7eb77e
zimolab <zimolab@aliyun.com>
2025-10-16 18:09:16 +0800

    update example code

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,37 +1,34 @@
-from typing import Literal
-
-from pyguiadapterlite import GUIAdapter, uprint
+from pyguiadapterlite import uprint, GUIAdapter
 
 
-def temperature_converter(
-    temperature: float,
-    from_unit: Literal["°C", "°F"] = "°F",
-    to_unit: Literal["°C", "°F"] = "°C",
-) -> float:
-    """
-    Convert temperature from one unit to another.
-    ---
-    *This is a simple demo of [`PyGUIAdapterLite`](https://github.com/zimolab/PyGUIAdapterLite).*
-
-    Args:
-        temperature (flot): current value of temperature
-        from_unit (str): current unit of temperature
-        to_unit (str):  target unit of temperature
-
-    Returns:
-        the converted value of temperature
-    """
-    if from_unit == "°C" and to_unit == "°F":
-        converted = temperature * 9 / 5 + 32
-    elif from_unit == "°F" and to_unit == "°C":
-        converted = (temperature - 32) * 5 / 9
-    else:
-        converted = temperature
-    uprint(f"{temperature}{from_unit} = {converted}{to_unit}")
-    return converted
+# step 0: define your function, here are some important points:
+# - annotate the parameters with their types using Python's type hints syntax
+# the GUIAdapter will automatically generate widgets for each parameter based on their types(by default)
+# - you can give the parameter a default value, the value will be used as the initial value of its widget
+def basic_types_demo(
+    int_value: int = 10,
+    float_value: float = 3.14,
+    str_value: str = "Hello, world!",
+    bool_value: bool = True,
+):
+    # uprint() is a function which is very similar to the built-in print()
+    # the difference is that it will not print the output to the stdout, instead,
+    # it will print it to the output area of the execute window
+    uprint("int_value:", int_value)
+    uprint("float_value:", float_value)
+    uprint("str_value:", str_value)
+    uprint("bool_value:", bool_value)
+    # you can return a value like any other function.
+    # this value will be captured by the execute window
+    # by default, the value will be printed to the output area
+    # and a popup window will be shown to display the value.
+    return "Function Done!"
 
 
 if __name__ == "__main__":
+    # step 1: create a GUIAdapter instance
     adapter = GUIAdapter()
-    adapter.add(temperature_converter)
+    # step 2: add your function to the adapter
+    adapter.add(basic_types_demo)
+    # step 3: run the adapter,
     adapter.run()
