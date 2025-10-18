40381479da54ee79d1338972d1ca4514a42b33c8
zimolab <zimolab@aliyun.com>
2025-10-18 11:28:49 +0800

    update example code

--- examples/str_types_demo.py
+++ examples/str_types_demo.py
@@ -3,11 +3,13 @@ from pyguiadapterlite.types import text_t, file_t, dir_t, color_hex_t
 
 
 def str_types_demo(
+    line_string: str = "This is a line string",
     long_string: text_t = "This is a long string",
     file_path: file_t = "/path/to/a/file.txt",
     dir_path: dir_t = "/path/to/a/directory",
     color_value: color_hex_t = "#FF0000",
 ):
+    uprint("Line string:", line_string)
     uprint("Long string:", long_string)
     uprint("File path:", file_path)
     uprint("Directory path:", dir_path)
