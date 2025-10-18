12b34550e59116a827cbf606e49faff3fb5a7efa
zimolab <zimolab@aliyun.com>
2025-10-18 23:02:29 +0800

    replace batch files with mkdoc.py

---
+++ mkdoc.py
@@ -0,0 +1,19 @@
+import shlex
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    action = sys.argv[1]
+    py = sys.executable
+    command = [py]
+    if action == "toc":
+        command.extend(["tools/mktoc.py", "README_draft.md", "-o", "README.md"])
+    elif action == "typedoc":
+        command.extend(
+            ["tools/mktypedoc.py", "--action", "save", "--output-file", "tmp.md"]
+        )
+    else:
+        print("Invalid action")
+        sys.exit(1)
+    print("Running command: ", shlex.join(command))
+    sys.exit(subprocess.call(command))
--- mkreadme.sh
+++
@@ -1,2 +0,0 @@
-python tools/mktoc.py README_draft.md -o README.md
-python tools/mktypedoc.py --action=save --output-file=tmp.md
