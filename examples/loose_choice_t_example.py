ff2247dfeb2ea77bc8de7263c0f40a34a74a7b16
zimolab <zimolab@aliyun.com>
2025-10-19 16:43:14 +0800

    add example

--- examples/loose_choice_t_example.py
+++ examples/loose_choice_t_example.py
@@ -1,14 +1,30 @@
 from pyguiadapterlite import uprint, GUIAdapter
-from pyguiadapterlite.types import loose_choice_t
+from pyguiadapterlite.types import loose_choice_t, LooseChoiceValue
 
 
-def loose_choice_example(
-    arg1: loose_choice_t = ("Option 1", "Option 2", "Option 3", "Option 4")
-):
+def loose_choice_example(arg1: loose_choice_t, arg2: loose_choice_t):
     uprint(f"arg1: {arg1}")
+    uprint(f"arg2: {arg2}")
 
 
 if __name__ == "__main__":
     adapter = GUIAdapter()
-    adapter.add(loose_choice_example)
+    adapter.add(
+        loose_choice_example,
+        arg1=LooseChoiceValue(
+            label="Your choice",
+            choices=["a", "b", "c"],
+            default_value="b",
+            readonly=False,
+            add_user_input=True,
+            description="Choose one of the options",
+        ),
+        arg2=LooseChoiceValue(
+            label="Another choice",
+            choices=["a", "b", "c"],
+            default_value="b",
+            readonly=True,
+            description="Choose one of the options",
+        ),
+    )
     adapter.run()
