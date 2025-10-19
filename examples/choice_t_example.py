5dba6a6166bb1239f2d0d834defbd97bb8cfc7c4
zimolab <zimolab@aliyun.com>
2025-10-19 16:31:55 +0800

    add example

--- examples/choice_t_example.py
+++ examples/choice_t_example.py
@@ -1,25 +1,35 @@
 from pyguiadapterlite import uprint, GUIAdapter
-from pyguiadapterlite.types import choice_t
-
-OPTIONS = {
-    "Python": 1,
-    "C/C++": 2,
-    "Java": 3,
-    "JavaScript": 4,
-    "C#": 5,
-    "Swift": 6,
-}
+from pyguiadapterlite.types import choice_t, SingleChoiceValue
 
 
-def choice_t_example(
-    choice_t_arg1: choice_t = ("choice1", "choice2", "choice3"),
-    choice_t_arg2: choice_t = OPTIONS,
-):
-    uprint(f"choice_t_arg1: {choice_t_arg1}, type: {type(choice_t_arg1)}")
-    uprint(f"choice_t_arg2: {choice_t_arg2}, type: {type(choice_t_arg2)}")
+def choice_t_example(arg1: choice_t, arg2: choice_t):
+    uprint(f"arg1: {arg1}, type: {type(arg1)}")
+    uprint(f"arg2: {arg2}, type: {type(arg2)}")
 
 
 if __name__ == "__main__":
     adapter = GUIAdapter()
-    adapter.add(choice_t_example)
+    adapter.add(
+        choice_t_example,
+        arg1=SingleChoiceValue(
+            label="Favorite language",
+            choices={
+                "Python": 1,
+                "C/C++": 2,
+                "Java": 3,
+                "JavaScript": 4,
+                "C#": 5,
+                "Swift": 6,
+            },
+            default_value=1,
+            columns=2,
+            hide_label=True,
+        ),
+        arg2=SingleChoiceValue(
+            label="Your choice",
+            choices=["Option 1", "Option 2", "Option 3", "Option 4"],
+            default_value="Option 2",
+            columns=1,
+        ),
+    )
     adapter.run()
