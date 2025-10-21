cd25e1ef84d25ea5dd0fce42452b9c912027ed59
zimolab <zimolab@aliyun.com>
2025-10-21 21:09:55 +0800

    add import statement

--- pyguiadapterlite/__init__.py
+++ pyguiadapterlite/__init__.py
@@ -1,5 +1,6 @@
 from pyguiadapterlite.core.adapter import GUIAdapter
 from pyguiadapterlite.core.fn import ParameterError
+from pyguiadapterlite.core.registry import ParameterWidgetFactory
 from pyguiadapterlite.windows.basewindow import BaseWindowConfig, BaseWindow
 from pyguiadapterlite.windows.fnexecwindow import FnExecuteWindowConfig, FnExecuteWindow
 from pyguiadapterlite.windows.fnselectwindow import FnSelectWindowConfig, FnSelectWindow
